1ccd23a30037e8b5adfe5b8412e73f4405fb61ac
Guy Hovav <guy.hovav@gmail.com>
2016-09-24 10:15:29 +0300

    Changing back to robohash, css'ing into place

--- src/components/card.js
+++ src/components/card.js
@@ -3,8 +3,8 @@ import React from 'react'
 export default ({data}) => {
     return(
         <div className="robot_card">
-            <img src={data.avatar}/>
-            <div>
+            <img className="avatar" src={data.avatar}/>
+            <div className = "info">
                 <h3>{data.first_name} from {data.country}</h3>
                 <p>{data.description}</p>
             </div>
--- src/components/profile.js
+++ src/components/profile.js
@@ -2,20 +2,21 @@ import React from 'react'
 
 export default (props) => {
     const {data, id} = props
-    const current_card = data[id-1];
+    const current_card = data[id - 1];
     return (
-        <div className="profile">
-            {current_card  ?
-                <div>
-                    <img src={current_card.avatar}/>
-                    <div>
-                        <h2>{current_card.first_name} {current_card.last_name}</h2>
-                        <h5>country: {current_card.country}</h5>
-                        <p>{current_card.description}</p>
-                    </div>
+        current_card ?
+            <div className="profile">
+                <div className="avatar_container">
+                    <img className="avatar" src={current_card.avatar.replace("size=80x80", "size=320x320") }/>
                 </div>
-                : null
-            }
-        </div>
+                <div className="info">
+                    <h2>{current_card.first_name} {current_card.last_name}</h2>
+                    <h5>country: {current_card.country}</h5>
+                    <p>{current_card.description}</p>
+                </div>
+            </div >
+            :
+            <div className="profile" />
+
     )
 }
